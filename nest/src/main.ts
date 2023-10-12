613cbfc8cdb616e91f45d7c287e07614f1d2a433
francismiko <1485059943@qq.com>
2023-10-12 17:27:20 +0800

    chore: port

--- nest/src/main.ts
+++ nest/src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(8000);
+  console.log(`Nest service is running on: ${await app.getUrl()}`);
 }
 bootstrap();
