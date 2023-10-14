c959bfabb85b7f2b92d0fe5dcee6bd4560140d87
francismiko <1485059943@qq.com>
2023-10-14 15:40:23 +0800

    feat: exception filter

--- nest/src/main.ts
+++ nest/src/main.ts
@@ -1,11 +1,13 @@
-import { NestFactory } from '@nestjs/core';
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { PrismaClientExceptionFilter } from './prisma-client-exception.filter';
 
 const PORT = process.env.PORT || 8000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   const swaggerConf = new DocumentBuilder()
     .setTitle('Title')
     .setDescription('Description')
@@ -13,6 +15,9 @@ async function bootstrap() {
     .build();
   const doc = SwaggerModule.createDocument(app, swaggerConf);
   SwaggerModule.setup('api', app, doc);
+
+  const { httpAdapter } = app.get(HttpAdapterHost);
+  app.useGlobalFilters(new PrismaClientExceptionFilter(httpAdapter));
 
   await app.listen(PORT);
   console.info(`🚀 Nest service is running on: ${await app.getUrl()}`);
---
+++ nest/src/prisma-client-exception.filter.ts
@@ -0,0 +1,12 @@
+import { ArgumentsHost, Catch } from '@nestjs/common';
+import { BaseExceptionFilter } from '@nestjs/core';
+import { Prisma } from '@prisma/client';
+
+@Catch(Prisma.PrismaClientKnownRequestError)
+export class PrismaClientExceptionFilter extends BaseExceptionFilter {
+  catch(exception: Prisma.PrismaClientKnownRequestError, host: ArgumentsHost) {
+    console.error(exception.message);
+
+    super.catch(exception, host);
+  }
+}
