fe656d25cb307aa42a3c01cf0b824bf6d4d60273
francismiko <1485059943@qq.com>
2023-10-22 15:08:13 +0800

    chore: packages

--- react/src/vite-env.d.ts
+++ react/src/vite-env.d.ts
@@ -1,10 +1,1 @@
 /// <reference types="vite/client" />
-
-export interface ImportMetaEnv {
-  readonly VITE_SUPABASE_URL: string;
-  readonly VITE_ANON_KEY: string;
-}
-
-export interface ImportMeta {
-  readonly env: ImportMetaEnv;
-}
