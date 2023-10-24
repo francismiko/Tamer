6e9e54630e26e5e75b933d82c73349d81034a948
francismiko <1485059943@qq.com>
2023-10-24 21:58:32 +0800

    feat: stepper

--- react/src/components/Layout.tsx
+++ react/src/components/Layout.tsx
@@ -1,7 +1,6 @@
 import { useAuth } from '@clerk/clerk-react';
 import React, { useEffect } from 'react';
 import { Outlet, useNavigate } from 'react-router-dom';
-import { Sidenav, initTE } from 'tw-elements';
 import { Content } from './Content';
 import { Loading } from './Loading';
 import { Sidebar } from './Sidebar';
@@ -9,10 +8,6 @@ import { Sidebar } from './Sidebar';
 export function Layout(): JSX.Element {
   const { isLoaded, isSignedIn } = useAuth();
   const navigate = useNavigate();
-
-  useEffect(() => {
-    initTE({ Sidenav });
-  }, []);
 
   useEffect(() => {
     if (isLoaded && !isSignedIn) {
--- react/src/components/Sidebar.tsx
+++ react/src/components/Sidebar.tsx
@@ -1,8 +1,13 @@
 import { UserButton } from '@clerk/clerk-react';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import { Ripple, Sidenav, initTE } from 'tw-elements';
 
 export function Sidebar(): JSX.Element {
+  useEffect(() => {
+    initTE({ Sidenav, Ripple });
+  }, []);
+
   return (
     <nav
       id="sidenav"
@@ -11,12 +16,13 @@ export function Sidebar(): JSX.Element {
       data-te-sidenav-mode="side"
       data-te-sidenav-hidden="false"
       data-te-sidenav-position="absolute"
+      data-te-sidenav-accordion="true"
     >
       <Link
         className="flex items-center justify-center py-6 mb-3 border-b-2 border-gray-100 border-solid outline-none"
         to="/"
         data-te-ripple-init
-        data-te-ripple-color="primary"
+        data-te-ripple-color="light"
       >
         <img
           className="w-48"
@@ -34,6 +40,8 @@ export function Sidebar(): JSX.Element {
             to="/dashboard"
             className="flex cursor-pointer items-center truncate rounded-[5px] px-6 py-[0.45rem] text-[0.85rem] text-gray-600 outline-none transition duration-300 ease-linear hover:bg-slate-50 hover:text-inherit hover:outline-none focus:bg-slate-50 focus:text-inherit focus:outline-none active:bg-slate-50 active:text-inherit active:outline-none data-[te-sidenav-state-active]:text-inherit data-[te-sidenav-state-focus]:outline-none motion-reduce:transition-none dark:text-gray-300 dark:hover:bg-white/10 dark:focus:bg-white/10 dark:active:bg-white/10"
             data-te-sidenav-link-ref
+            data-te-ripple-init
+            data-te-ripple-color="light"
           >
             <span className="mr-4 [&>svg]:h-3.5 [&>svg]:w-3.5 [&>svg]:text-gray-400 dark:[&>svg]:text-gray-300">
               <svg
@@ -54,6 +62,8 @@ export function Sidebar(): JSX.Element {
             to="/dashboard/chat"
             className="flex cursor-pointer items-center truncate rounded-[5px] px-6 py-[0.45rem] text-[0.85rem] text-gray-600 outline-none transition duration-300 ease-linear hover:bg-slate-50 hover:text-inherit hover:outline-none focus:bg-slate-50 focus:text-inherit focus:outline-none active:bg-slate-50 active:text-inherit active:outline-none data-[te-sidenav-state-active]:text-inherit data-[te-sidenav-state-focus]:outline-none motion-reduce:transition-none dark:text-gray-300 dark:hover:bg-white/10 dark:focus:bg-white/10 dark:active:bg-white/10"
             data-te-sidenav-link-ref
+            data-te-ripple-init
+            data-te-ripple-color="light"
           >
             <span className="mr-4 [&>svg]:h-5 [&>svg]:w-5 [&>svg]:text-gray-400 dark:[&>svg]:text-gray-300">
               <svg
@@ -73,6 +83,60 @@ export function Sidebar(): JSX.Element {
             </span>
             <span>Chat</span>
           </Link>
+        </li>
+        <li className="relative">
+          <a
+            className="flex h-12 cursor-pointer items-center truncate rounded-[5px] px-6 py-4 text-[0.875rem] text-gray-600 outline-none transition duration-300 ease-linear hover:bg-slate-50 hover:text-inherit hover:outline-none focus:bg-slate-50 focus:text-inherit focus:outline-none active:bg-slate-50 active:text-inherit active:outline-none data-[te-sidenav-state-active]:text-inherit data-[te-sidenav-state-focus]:outline-none motion-reduce:transition-none dark:text-gray-300 dark:hover:bg-white/10 dark:focus:bg-white/10 dark:active:bg-white/10"
+            data-te-sidenav-link-ref
+            data-te-ripple-init
+            data-te-ripple-color="light"
+          >
+            <span className="mr-4 [&>svg]:h-4 [&>svg]:w-4 [&>svg]:text-gray-400 dark:[&>svg]:text-gray-300">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+                className="w-5 h-5"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M10 2c-2.236 0-4.43.18-6.57.524C1.993 2.755 1 4.014 1 5.426v5.148c0 1.413.993 2.67 2.43 2.902 1.168.188 2.352.327 3.55.414.28.02.521.18.642.413l1.713 3.293a.75.75 0 001.33 0l1.713-3.293a.783.783 0 01.642-.413 41.102 41.102 0 003.55-.414c1.437-.231 2.43-1.49 2.43-2.902V5.426c0-1.413-.993-2.67-2.43-2.902A41.289 41.289 0 0010 2zM6.75 6a.75.75 0 000 1.5h6.5a.75.75 0 000-1.5h-6.5zm0 2.5a.75.75 0 000 1.5h3.5a.75.75 0 000-1.5h-3.5z"
+                  clip-rule="evenodd"
+                />
+              </svg>
+            </span>
+            <span>Conversations</span>
+            <span
+              className="absolute right-0 ml-auto mr-[0.8rem] transition-transform duration-300 ease-linear motion-reduce:transition-none [&>svg]:text-gray-600 dark:[&>svg]:text-gray-300"
+              data-te-sidenav-rotate-icon-ref
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+                className="h-5 w-5"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z"
+                  clip-rule="evenodd"
+                />
+              </svg>
+            </span>
+          </a>
+          <ul
+            className="!visible relative m-0 hidden list-none p-0 data-[te-collapse-show]:block "
+            data-te-sidenav-collapse-ref
+          >
+            <li className="relative">
+              <a
+                className="flex h-6 cursor-pointer items-center truncate rounded-[5px] py-4 pl-[3.4rem] pr-6 text-[0.78rem] text-gray-600 outline-none transition duration-300 ease-linear hover:bg-slate-50 hover:text-inherit hover:outline-none focus:bg-slate-50 focus:text-inherit focus:outline-none active:bg-slate-50 active:text-inherit active:outline-none data-[te-sidenav-state-active]:text-inherit data-[te-sidenav-state-focus]:outline-none motion-reduce:transition-none dark:text-gray-300 dark:hover:bg-white/10 dark:focus:bg-white/10 dark:active:bg-white/10"
+                data-te-sidenav-link-ref
+              >
+                Link 2
+              </a>
+            </li>
+          </ul>
         </li>
       </ul>
       <div className="absolute bottom-0 flex items-center justify-center w-full h-32 p-4 text-center bg-inherit">
