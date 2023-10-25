667a94797d4cadb33bb64d03431d6447d13f4a32
francismiko <1485059943@qq.com>
2023-10-25 15:41:33 +0800

    chore: schema

--- nest/src/modules/app.module.ts
+++ nest/src/modules/app.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule, loggingMiddleware } from 'nestjs-prisma';
 import { MessageModule } from './message/message.module';
-import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
-    UserModule,
     MessageModule,
     PrismaModule.forRoot({
       isGlobal: true,
